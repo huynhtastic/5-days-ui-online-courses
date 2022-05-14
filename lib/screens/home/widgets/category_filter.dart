d90acc2abbf9a41d50a5b9c6c23ab44249892b2c
Richard Huynh <huynhtastic@gmail.com>
2022-05-14 17:59:25 -0500

    finish categories functionality

--- lib/screens/home/widgets/category_filter.dart
+++ lib/screens/home/widgets/category_filter.dart
@@ -6,7 +6,7 @@ class CategoryFilter extends StatefulWidget {
 }
 
 class _CategoryFilterState extends State<CategoryFilter> {
-  final categories = [
+  var categories = [
     'Productivity',
     'Art',
     'Design',
@@ -24,23 +24,34 @@ class _CategoryFilterState extends State<CategoryFilter> {
       children: [
         Text(
           'Browse by categories',
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 24.0),
         Wrap(
           spacing: 8,
           runSpacing: 8,
           children: categories.map((category) {
+            final selected = filters.contains(category);
             return FilterChip(
+              showCheckmark: false,
               padding: EdgeInsets.all(12),
-              onSelected: (m) => {print(m)},
-              backgroundColor: Color.fromRGBO(0, 0, 0, 0.05),
+              selected: selected,
+              onSelected: (val) {
+                setState(() {
+                  val
+                      ? filters.add(category)
+                      : filters.removeWhere((filtered) => category == filtered);
+                });
+              },
+              backgroundColor: const Color.fromRGBO(0, 0, 0, 0.05),
+              selectedColor: Colors.white,
               label: Text(
                 category,
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(0, 0, 75, 0.5),
-                  // color: Color.fromRGBO(210, 120, 225, 1),
+                  color: selected
+                      ? const Color.fromRGBO(210, 120, 225, 1)
+                      : const Color.fromRGBO(0, 0, 75, 0.5),
                 ),
               ),
             );
